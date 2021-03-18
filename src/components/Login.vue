d1c72406de80d576a24d091a4fb37c1ea59540d2
Dario Miskovic <47636050+darioMiskovic@users.noreply.github.com>
2021-03-18 18:08:57 +0100

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -33,8 +33,8 @@ export default {
     data(){
         return{
             //promjeniti!!!!!!**************
-            email: 'alumomce@gmail.com',
-            password: 'arsenal'
+            email: '*******',
+            password: '*******'
         }
     },
     methods:{
